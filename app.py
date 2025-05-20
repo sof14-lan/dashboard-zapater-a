3cc4c6cddbe0390b95ff3b6facdd978885b52588
sof14-lan <tanniasofiahm@gmail.com>
2025-05-20 11:15:06 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-import plotly.express as px
+import pyplot.express as px
 
 # Configuración de la página
 st.set_page_config(page_title="Dashboard de Inteligencia Comercial", layout="wide")
